0ead90e7980ed899b91abd452181b9022c4346d9
ManuelHMR <manuelhmrjf@gmail.com>
2023-01-17 11:38:26 -0300

    feat: get all product links of a brand

--- src/services/searchServices.ts
+++ src/services/searchServices.ts
@@ -1,6 +1,5 @@
-import puppetter from "puppeteer";
 import { webScrapingUtil } from "../utils/puppeteer";
 
 export async function searchService(brand: string){
-    return webScrapingUtil();
+    return await webScrapingUtil(brand);
 };
--- src/utils/puppeteer.ts
+++ src/utils/puppeteer.ts
@@ -1,10 +1,14 @@
 import puppeteer from "puppeteer";
 
-export async function webScrapingUtil() {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
+export async function webScrapingUtil(brand: string) {
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://oxylabs.io/');
-    // await browser.close();
+    await page.goto('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+    await page.waitForSelector(".thumbnail");
+    const linksWithNulls = await page.$$eval('h4 > a', (element, brand) => element.map((a) => {
+        if(a.textContent !== null && a.textContent.includes(brand)) return a.href
+    }), brand);
+    const links = linksWithNulls.filter(link=>link!== null);
+    await browser.close();
+    return links;
 };
